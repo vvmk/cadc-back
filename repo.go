4822eff1ec8a7943fba62634557e9fb8fe7427a3
vvmk <vincentmasiello@gmail.com>
2018-06-20 00:08:16 -0400

    redigo experiments

--- repo.go
+++ repo.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func NewPost(post Post) {
+	pool := NewPool()
+	c := pool.Get()
+	defer c.Close()
+
+	i, err := c.Do("INCR", "post:id")
+	if err != nil {
+		panic(err)
+	}
+
+	prin
+	k := "post:" + strconv.Itoa(i)
+	fmt.Println(k)
+
+	c.Send("SET", k, post)
+
+	r, err := c.Do("EXEC")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(r)
+}
